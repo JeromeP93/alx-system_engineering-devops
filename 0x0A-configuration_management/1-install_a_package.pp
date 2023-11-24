7fac359db6f5bad3ea609faf171c127a0f6869f7
Jerome S. Patrick <jeromespatrick@gmail.com>
2023-11-24 16:33:12 +0000

    task 1 update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+#!/usr/bin/puppet
+
+# Install pip3 package
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install a specific version of Flask (2.1.0)
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'], # Ensure pip3 is installed first
 }
